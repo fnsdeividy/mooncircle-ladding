060df655bec6f9488571a4221e5b94a992553591
jhonattan ferri <jhoontutors@gmail.com>
2023-08-15 02:42:29 -0300

    preparando o footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { FooterContaier } from './styles';
+import {AiOutlineInstagram, AiFillYoutube, AiOutlineCopyright, AiOutlineTwitter} from 'react-icons/ai'
+import MoonCircle from '../../assets/MoonCircle.png'
+import Imagem1 from '../../assets/image 1.png'
+import Imagem2 from '../../assets/image 2.png'
+
+const Footer = () => {
+  return (
+    <FooterContaier>
+      <div className='Footer-moon'>
+      <img  src={MoonCircle} alt="Footer Imagem" />
+      <p className='copyright'>Copyright<AiOutlineCopyright/>2023</p>
+      <p className='direitos'>Todos os direitos reservados</p>
+        <AiOutlineInstagram className='instagram'/>
+       <AiOutlineTwitter className='twitter'/>
+       <AiFillYoutube className='youtube'/>
+      </div>
+      
+      <div className="pages">
+        <h1 className='paginas'>Páginas</h1>
+        <p className='paginas-home'>Home</p>
+        <p className='paginas-Saiba-mais'>Saiba mais</p>
+        <p className='paginas-Parceiros'>Parceiros</p>
+        <p className='paginas-Blog'>Blog</p>
+      </div>
+
+      <div className="contato">
+        <h1 className='contato-h1'>Contato</h1>
+        <p className='email'>moon@mooncircle</p>
+        <p className='telefone'>(00)000000000</p>
+      </div>
+      
+      <div className="outras-imagens">
+        <h1 className='download'>Em breve para download:</h1>
+        <div className='images'>
+        <img className='image-1' src={Imagem1} alt="Imagem 1" />
+        <img className='image-2' src={Imagem2} alt="Imagem 2" />
+        </div>
+      </div>
+    </FooterContaier>
+  );
+};
+
+export default Footer;
---
+++ src/components/footer/styles.ts
@@ -0,0 +1,201 @@
+import styled from "styled-components";
+
+
+export const FooterContaier = styled.footer`
+    display: flex;
+  align-items: center;
+  justify-content: center;
+.pages .header {
+    display: none;
+  }
+.Footer-moon {
+    display: block;
+    position: relative;
+    width: 201px;
+  height: 52px;
+  top: 100px;
+  left: -140px;
+
+  
+
+  .copyright {
+    color: white;
+  }
+  .direitos {
+    color: white;
+
+  }
+  .instagram {
+    width: 24px;
+  height: 24px;
+  padding: 1.5px;
+  color: white;
+  }
+  .twitter {
+    width: 24px;
+    height: 24px;
+    padding: 2.9998321533203125px 0.75px 3.000112533569336px 0.75px;
+    color: white;
+  }
+  .youtube {
+    width: 24px;
+  height: 24px;
+  padding: 3px 0.0016617532819509506px 2.99951171875px 0.0013652089983224869px;
+  color: white;
+  }
+}
+.pages {
+  display: block;
+  position: relative;
+  top: 150px;
+  left: 45px;
+
+.paginas {
+  width: 160px;
+height: 28px;
+font-family: Museo Sans;
+font-size: 20px;
+font-weight: 600;
+line-height: 28px;
+letter-spacing: 0em;
+text-align: left;
+color: #FFFFFF;
+}
+.paginas-home {
+  width: 160px;
+  height: 20px;
+  font-family: Museo Sans;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 20px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #F5F7FA;
+}
+.paginas-Saiba-mais {
+  width: 160px;
+height: 20px;
+font-family: Museo Sans;
+font-size: 14px;
+font-weight: 400;
+line-height: 20px;
+letter-spacing: 0em;
+text-align: left;
+color: #F5F7FA;
+
+}
+.paginas-Parceiros {
+  width: 160px;
+height: 20px;
+font-family: Museo Sans;
+font-size: 14px;
+font-weight: 400;
+line-height: 20px;
+letter-spacing: 0em;
+text-align: left;
+color: #F5F7FA;
+
+}
+.paginas-Blog {
+  width: 160px;
+height: 20px;
+font-family: Museo Sans;
+font-size: 14px;
+font-weight: 400;
+line-height: 20px;
+letter-spacing: 0em;
+text-align: left;
+color: #F5F7FA;
+
+}
+  }
+
+.contato {
+    display: block;
+  position: relative;
+  top: 150px;
+  left: 45px;
+
+.contato-h1 {
+  display: block;
+  position: relative;
+  top: -16px;
+  width: 160px;
+height: 20px;
+font-family: Museo Sans;
+font-size: 20px;
+font-weight: 600;
+line-height: 28px;
+letter-spacing: 0em;
+text-align: left;
+
+color: #F5F7FA;
+
+
+}
+.email {
+  width: 160px;
+height: 20px;
+font-family: Museo Sans;
+font-size: 14px;
+font-weight: 400;
+line-height: 20px;
+letter-spacing: 0em;
+text-align: left;
+color: #F5F7FA;
+
+}
+
+.telefone {
+  width: 160px;
+height: 20px;
+font-family: Museo Sans;
+font-size: 14px;
+font-weight: 400;
+line-height: 20px;
+letter-spacing: 0em;
+text-align: left;
+color: #F5F7FA;
+
+}
+  }
+
+.outras-imagens {
+  display: block;
+  position: relative;
+  top: 150px;
+  left: 45px;
+  
+.download {
+  width: 239px;
+height: 20px;
+font-family: Museo Sans;
+font-size: 20px;
+font-weight: 600;
+line-height: 20px;
+letter-spacing: 0em;
+text-align: left;
+color: #FFFFFF;
+
+}
+.images {
+  width: 394px;
+height: 53px;
+gap: 32px;
+
+.image-1 {
+  width: 181.00000447034745px;
+height: 53.000000655650865px;
+padding: 5px;
+}
+.image-2 {
+  width: 181.00000447034745px;
+height: 53.000000655650865px;
+padding: 5px;
+
+
+}
+}
+
+} 
+`
