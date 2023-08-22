a16dd481ee213743132628ec5e1dd890a5490460
jhonattan ferri <jhoontutors@gmail.com>
2023-08-22 10:50:22 -0300

    responsivo feito

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,17 +1,13 @@
 import React, { useState } from 'react'
-import { Container, Math, Confirm } from './styles'
-import ConfirmationButton from '../../components/Button'
+import { Container } from './styles'
+
 import {Input, PhoneNumberInput} from '../../components/Input'
 import LogoHome from '../../assets/LogoHome.png'
 const Home = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [whatsapp, setWhatsapp] = useState('');
-  function handleConfirm(): void {
-    throw new Error('Function not implemented.')
-  }
-
-
+ 
   const handleNameChange = (e: { target: { value: React.SetStateAction<string> } }) => {
     setName(e.target.value);
   };
@@ -26,19 +22,25 @@ const Home = () => {
 
   return (
     <Container>
-        <Math>
+        <div className='Math'>
         <h1 >Bem-vinda ao Moon Circle.</h1>
         <h4 >O seu próximo match é aqui!</h4>
         <p >Segurança para viver e liberdade para ser. Mergulhe no Universo Moon Circle.
         Quer participar? Entre na fila de embarque
         </p>
-        </Math>
-        <Confirm>
-        <Input  value={name} onChange={handleNameChange} placeholder="Digite seu nome"   />
+        </div>
+        <div className='Input'>
+        <Input   value={name} onChange={handleNameChange} placeholder="Digite seu nome"   />
         <Input  value={email} onChange={handleEmailChange} placeholder="Digite seu email"   />
         <PhoneNumberInput  value={whatsapp} onChange={handleWhatsappChange} placeholder="Digite seu número de WhatsApp"   />
-        <ConfirmationButton  onConfirm={handleConfirm} text="Confirmar Inscrição" children={''} />
-        </Confirm>
+        </div>
+        <div className='Confirm'>
+        <div className='ContainerButton'>
+        <button>
+          <p className='confirm'>Confirmar inscrição</p>
+      </button>
+      </div>
+        </div>
           <img className='Logo-Home' src={LogoHome} alt="" />
     </Container> 
   )
--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+const mobileBreakpoint = "500px";
 
 export const Container = styled.div`
   display: flex;
@@ -15,24 +15,16 @@ left: -5rem;
         position: relative;
         width: 400px;
         height: 400px;
-        
-       
         top: -40rem;
         left: 30rem;
         padding: 0;
     }
-`
 
-
-
-export const Math = styled.div`
-    
+.Math {
     gap: 32px;
-
     h1 {
         width: 269px;
         height: 23px;
-
         font-family: Museo Sans;
         font-size: 20px;
         font-weight: 600;
@@ -40,10 +32,7 @@ export const Math = styled.div`
         letter-spacing: 0.03em;
         text-align: left;
         color: #FFFFFF;
-        
-
     }
-
     h4 {
         width: 527px;
         height: 152px;
@@ -54,9 +43,7 @@ export const Math = styled.div`
         letter-spacing: 0em;
         text-align: left;
         color: #FFFFFF;
-
     }
-
     p {
         width: 527px;
         height: 120px;
@@ -67,15 +54,141 @@ export const Math = styled.div`
         letter-spacing: 0em;
         text-align: left;
         color: #FFFFFF;
-
     }
-
-    
-    `
-export const Confirm = styled.div`
+}
+.Confirm{
     width: 407.55px;
     height: 334.19px;
-    gap: 24px;
+    .ContainerButton {
+        display: flex;
+    position: relative;
+        button {
+      width: 223px;
+    height: 50px;
+    padding: 6px 0px 8px 29px;
+border-radius: 100px;
+gap: 10px;
+background: #641978;
+box-shadow: 0px 10px 100px 0px #00000040;
+}
+.confirmP {
+    width: 210px;
+height: 34px;
+font-family: Museo Sans;
+font-size: 20px;
+font-weight: 600;
+line-height: 32px;
+letter-spacing: 0em;
+text-align: left;
+color: #FFFFFF;
+}
+    }
+} 
 
+@media (max-width: ${mobileBreakpoint}) {
+    display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+top  : 1rem; 
+left: -5rem;
+    .Logo-Home {
+        display: flex;
+    position: relative;
+    width: 230px;
+    height: 230px;
+    top: -52rem;
+    left: 5rem;
+    padding: 0;
+    }
+
+.Math {
+    gap: 32px;
+    h1 {
+        width: 269px;
+        height: 23px;
+        font-family: Museo Sans;
+        font-size: 20px;
+        font-weight: 600;
+        line-height: 26px;
+        letter-spacing: 0.03em;
+        text-align: left;
+        color: #FFFFFF;
+        position: relative;
+    display: flex;
+    top: 6.5rem;
+    left: 5.5rem;
+    }
+    h4 {
+        width: 226px;
+    height: 73px;
+    font-family: Museo Sans;
+    font-size: 33px;
+    font-weight: 600;
+    line-height: 32px;
+    letter-spacing: 0em;
+    text-align: left;
+    color: #FFFFFF;
+    position: relative;
+    display: flex;
+    top: 7rem;
+    left: 6rem;
+    }
+    p {
+        width: 272px;
+    height: 85px;
+    font-family: Museo Sans;
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 24px;
+    letter-spacing: 0em;
+    text-align: left;
+    color: #FFFFFF;
+    display: flex;
+    position: relative;
+    left: 5.5rem;
+    top: 7rem;
+    }
+}
+.Input {
+    display: grid;
+    position: relative;
+    left: 5rem;
+    top: 8rem;
+}
+.Confirm{
+    width: 407.55px;
+    height: 354.19px;
     
+   
+    .ContainerButton {
+        display: flex;
+    position: relative;
+    left: 10.5rem;
+    top: 8.5rem;
+        button {
+      width: 223px;
+    height: 50px;
+    padding: 6px 0px 8px 29px;
+border-radius: 100px;
+gap: 10px;
+background: #641978;
+box-shadow: 0px 10px 100px 0px #00000040;
+}
+.confirmP {
+    width: 210px;
+height: 34px;
+font-family: Museo Sans;
+font-size: 20px;
+font-weight: 600;
+line-height: 32px;
+letter-spacing: 0em;
+text-align: left;
+color: #FFFFFF;
+}
+    }
+} 
+
+}
 `
