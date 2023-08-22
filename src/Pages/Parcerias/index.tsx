b9cb243b6d9aad56ae1e590e8abfc5a814c25e1f
jhonattan ferri <jhoontutors@gmail.com>
2023-08-21 21:34:22 -0300

    estilizacao e responsivo parceerias

--- src/Pages/Parcerias/index.tsx
+++ src/Pages/Parcerias/index.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import Haya from '../../assets/Ellipse 6.png'
 import Camara from '../../assets/Frame 64.png'
 import Bold from '../../assets/Ellipse 8.png'
 import Castro from '../../assets/Ellipse 9.png'
+import Grafismo from '../../assets/Grafismos-degrade 2.png'
 import { ContainerParcerias } from './styles'
+import  { InputNomeEstabelecimento, InputNomeInstagram } from '../../components/Input'
 
 const Parcerias = () => {
+  const [establishmentName, setEstablishmentName] = useState('');
+  const [instagramUsername, setInstagramUsername] = useState('');
+
+  function handleEstablishmentNameChange(event: ChangeEvent<HTMLInputElement>): void {
+    const establishment = event.target.value;
+    setEstablishmentName(establishment);
+  }
+
+  function handleInstagramUsernameChange(event: ChangeEvent<HTMLInputElement>): void {
+    const Instagram = event.target.value;
+    setInstagramUsername(Instagram);
+  }
+
   return (
     <ContainerParcerias>
       <div className='ContainerText'>
@@ -18,18 +33,37 @@ const Parcerias = () => {
       <img className='Image3' src={Bold} alt="" />
       <img className='Image4' src={Castro} alt="" />
       </div>
-      <div className='ContainerText'>
+      <div className='ContainerTexts'>
       <h1>A construção do nosso universo precisa da sua ajuda!</h1>
       <p>Indique aqui um estabelecimento para fazer parte do Moon Circle. Pode ser bar, restaurante, hotel, balada, companhia aérea, entre outros!
         Lembrando que para fazer parte da nossa tripulação, o local precisa ser livre de qualquer tipo de preconceito.
       </p>
       </div>
       <div className='ContainerInput'>
-      <input />
-      <input />
-      <button />
-      <h3>Quer ser nosso parceiro?</h3>
-      <p>Envie um e-mail para moon@mooncircle.app </p>
+      <div className='PrimeiroInput'>
+        <InputNomeEstabelecimento 
+          placeholder=""
+          onChange={handleEstablishmentNameChange}
+          value={establishmentName}
+        />
+      </div>
+      <div className='SegundoInput'>
+        <InputNomeInstagram
+          placeholder=""
+          onChange={handleInstagramUsernameChange}
+          value={instagramUsername}
+        />
+      </div>
+        <div className='ButtonForm'>
+      <button>
+          <p>Enviar indicação</p>
+      <img className='ImageGrafismo' src={Grafismo} alt="" />
+      </button>
+       </div>
+       <div className='containerprojetos'>
+      <h3 className='Parceiroh3'>Quer ser nosso parceiro?</h3>
+      <p className='email'>Envie um e-mail para moon@mooncircle.app </p>
+      </div>
       </div>
     </ContainerParcerias>
   )
--- src/Pages/Parcerias/styles.ts
+++ src/Pages/Parcerias/styles.ts
@@ -1,25 +1,22 @@
 import styled from "styled-components";
 
-
+const mobileBreakpoint = "600px";
 
 export const ContainerParcerias = styled.div`
 display: flex;
   align-items: center;
   justify-content: center;
   color: white;
-  
   .ContainerText{
     display: flex;
     position: relative;
-    
-
-
   }
+
   .TextParceiros {
    display: flex;
    position: relative;
-   top: -53rem;
-    left: 40rem;
+   top: -70rem;
+   left: 100rem;
     width: 17pc;
     font-family: Museo Sans;
     font-size: 36px;
@@ -41,12 +38,18 @@ display: flex;
     color: white;
     display: flex;
     position: relative;
-    top: -49rem;
-    left: 20rem;
+    top: -70rem;
+    left: 97.5rem
   }
   .ContainerImages {
     display: flex;
+    left: 60rem;
+    top: -59rem;
     position: relative;
+    flex-direction: initial;
+    justify-content: center;
+    align-items: center;
+    
   }
   .Image1 {
     width: 200px;
@@ -64,4 +67,339 @@ height: 200px;
     width: 200px;
 height: 200px;
   }
+
+  .ContainerTexts {
+    display: flex;
+   position: relative;
+   top: -10rem;
+   left: -46rem;
+
+    h1 {
+      width: 481px;
+    height: 77px;
+    font-family: Museo Sans;
+    font-size: 32px;
+    font-weight: 600;
+    line-height: 44px;
+    letter-spacing: 0em;
+    text-align: left;
+    color: white;
+    display: flex;
+    position: relative;
+    top: -36rem;
+    left: 78rem;
+    }
+
+    p {
+      width: 431px;
+    height: 80px;
+    font-family: Inter;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 20px;
+    letter-spacing: 0em;
+    text-align: left;
+    color: white;
+    display: flex;
+    position: relative;
+    top: -30.5rem;
+    left: 48rem;
+    }
+  }
+  .ContainerInput {
+    display: flex;
+   position: relative;
+   top: -10rem;
+    left: -55rem;
+    gap: 16px;
+
+    .PrimeiroInput {
+      display: flex;
+      position: relative;
+      top: -24rem;
+      left: 30rem;
+
+    }
+    .SegundoInput {
+      display: flex;
+    position: relative;
+    top: -20rem;
+    left: 12.8rem;
+    }
+    button {
+      width: 223px;
+    height: 50px;
+    padding: 8px 0px 8px 43px;
+border-radius: 100px;
+gap: 10px;
+background: #641978;
+box-shadow: 0px 10px 100px 0px #00000040;
+
+p {
+  width: 154px;
+height: 32px;
+font-family: Museo Sans;
+font-size: 20px;
+font-weight: 600;
+line-height: 32px;
+letter-spacing: 0em;
+text-align: left;
+color: #FFFFFF;
+
+}
+
+.ImageGrafismo {
+  width: 470px;
+    height: 470px;
+    display: flex;
+    position: relative;
+    top: -25rem;
+    left: -38rem;
+  }
+    }
+    .ButtonForm {
+      display: flex;
+      position: relative;
+      left: -3rem;
+    top: -16rem;
+
+  }
+
+  
+ .containerprojetos {
+  .Parceiroh3 {
+    width: 424px;
+    height: 2px;
+    font-family: Museo Sans;
+    font-size: 40px;
+    font-weight: 600;
+    line-height: 44px;
+    letter-spacing: 0em;
+    text-align: left;
+    color: white;
+    display: flex;
+    position: relative;
+    top: 6rem;
+    left: -38rem;
+
+  }
+
+  .email {
+    width: 34rem;
+    height: 38px;
+    font-family: Inter;
+    font-size: 30px;
+    font-weight: 400;
+    line-height: 20px;
+    letter-spacing: 0em;
+    text-align: left;
+    color: white;
+    display: flex;
+    position: relative;
+    top: 10rem;
+    left: -42rem;
+
+
+  }
+ }
+}
+@media (max-width: ${mobileBreakpoint}) {
+  .ContainerText{
+    display: flex;
+    position: relative;
+  }
+
+  .TextParceiros {
+    display: flex;
+    position: relative;
+    top: -69rem;
+    left: 82.7rem;
+    width: 21pc;
+    font-family: Museo Sans;
+    font-size: 39px;
+    font-weight: 600;
+    line-height: 44px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: white;
+  }
+  .TextP{
+    width: 313px;
+    height: 24px;
+    font-family: Museo Sans;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 24px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: white;
+    display: flex;
+    position: relative;
+    top: -65.5rem;
+    left: 61.5rem;
+  }
+  .ContainerImages {
+    display: flex;
+    left: 41.5rem;
+    top: -59rem;
+    position: relative;
+    flex-direction: initial;
+    justify-content: center;
+    align-items: center;
+    
+  }
+  .Image1 {
+    width: 75px;
+height: 75px;
+  }
+  .Image2 {
+    width: 75px;
+height: 75px;
+  }
+  .Image3 {
+    width: 75px;
+height: 75px;
+  }
+  .Image4 {
+    width: 75px;
+height: 75px;
+  }
+
+  .ContainerTexts {
+    display: flex;
+   position: relative;
+   top: -10rem;
+   left: -46rem;
+
+    h1 {
+      width: 254px;
+    height: 77px;
+    font-family: Museo Sans;
+    font-size: 26px;
+    font-weight: 600;
+    line-height: 44px;
+    letter-spacing: 0.03em;
+    text-align: left;
+    color: white;
+    display: flex;
+    position: relative;
+    top: -43rem;
+    left: 71rem;
+
+
+    }
+
+    p {
+      width: 400px;
+    height: 80px;
+    font-family: Inter;
+    font-size: 23px;
+    font-weight: 400;
+    line-height: 32px;
+    letter-spacing: 0em;
+    text-align: left;
+    color: white;
+    display: flex;
+    position: relative;
+    top: -31.5rem;
+    left: 50.5rem;
+    }
+  }
+  .ContainerInput {
+    display: flex;
+   position: relative;
+   top: -10rem;
+    left: -55rem;
+    gap: 16px;
+
+    .PrimeiroInput {
+      display: flex;
+      position: relative;
+      top: -17rem;
+    left: 37.5rem;
+
+    }
+    .SegundoInput {
+      display: flex;
+    position: relative;
+    top: -12rem;
+    left: 20.3rem;
+
+    }
+    button {
+      width: 223px;
+    height: 50px;
+    padding: 8px 0px 8px 43px;
+border-radius: 100px;
+gap: 10px;
+background: #641978;
+box-shadow: 0px 10px 100px 0px #00000040;
+
+p {
+  width: 154px;
+height: 32px;
+font-family: Museo Sans;
+font-size: 20px;
+font-weight: 600;
+line-height: 32px;
+letter-spacing: 0em;
+text-align: left;
+color: #FFFFFF;
+
+}
+
+.ImageGrafismo {
+  display: none;
+  }
+    }
+    .ButtonForm {
+      display: flex;
+      position: relative;
+      left: 3.9rem;
+    top: -5rem;
+
+  }
+
+  
+ .containerprojetos {
+  .Parceiroh3 {
+    width: 424px;
+    height: 2px;
+    font-family: Museo Sans;
+    font-size: 40px;
+    font-weight: 600;
+    line-height: 55px;
+    letter-spacing: 0em;
+    text-align: left;
+    color: white;
+    display: flex;
+    position: relative;
+    top: -1rem;
+    left: -12rem;
+
+
+  }
+
+  .email {
+    width: 34rem;
+    height: 38px;
+    font-family: Inter;
+    font-size: 30px;
+    font-weight: 400;
+    line-height: 30px;
+    letter-spacing: 0em;
+    text-align: left;
+    color: white;
+    display: flex;
+    position: relative;
+    top: 7rem;
+    left: -12rem;
+
+
+  }
+ }
+  }}
+
+
+
 `
