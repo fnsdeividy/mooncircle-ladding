eb4c7802278cc0b4eb1d8889e55e2aa70029b867
jhonattan ferri <jhoontutors@gmail.com>
2023-08-22 16:12:29 -0300

    responsivo concluido

--- src/Pages/SaibaMais/styles.ts
+++ src/Pages/SaibaMais/styles.ts
@@ -3,35 +3,33 @@ import styled from "styled-components";
 export const Container = styled.div`
   position: relative;
   width: 100%;
-  min-height: 92vh; 
+  flex-direction: column;
+    justify-content: center;
+    align-items: center;
   display: flex;
-  position: relative;
 
   img {
-    width: 750px;
-    height: 750px;
+    width: 600px;
+    height: 600px;
    display: flex;
    position: relative;
-  top: -30rem;
+   top: -25rem;
+  left: -22rem;
   }
-`;
 
-
-
-export const Content = styled.div`
-  display: flex;
+  .Content {
+    display: flex;
   position: relative;
-  top: -25rem;
-  left: -27rem;
+  top: -56rem;
+  left: -10rem;
   flex-direction: column;
   align-items: center;
   box-sizing: border-box;
   width: 601px;
 height: 542px;
+  }
 
-`;
-
-export const Frame = styled.div`
+  .Frame{
     width: 100%;
   max-width: 800px;
   padding: 0px;
@@ -52,5 +50,63 @@ export const Frame = styled.div`
     font-size: 16px;
     line-height: 1.5;
   }
+  }
 
-`
+  @media (max-width: 600px) {
+    position: relative;
+  width: 100%;
+  flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  display: flex;
+
+  img {
+    width: 200px;
+    height: 200px;
+    display: flex;
+    position: relative;
+    top: -23rem;
+    left: 1rem;
+  }
+
+  .Content {
+    display: flex;
+    position: relative;
+    top: -24rem;
+    left: -23rem;
+    flex-direction: column;
+    align-items: center;
+    box-sizing: border-box;
+    width: 318px;
+    height: 542px;
+  }
+
+  .Frame{
+    width: 100%;
+  max-width: 800px;
+  padding: 0px;
+ 
+  color: #ffffff;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  
+
+  margin-right: -47rem;
+
+  h1 {
+    font-size: 24px;
+    font-weight: 600;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-size: 16px;
+    line-height: 1.5;
+  }
+  }
+  }
+`;
+
+
+
+
+
